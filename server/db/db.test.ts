26d36bda54f53034d09b6d30874ade6695b95f6e
Regie Malonzo <regiemalonzo@gmail.com>
2024-06-20 11:00:19 +1200

    create test for events

--- server/db/db.test.ts
+++ server/db/db.test.ts
@@ -1,6 +1,9 @@
 import { describe, it, expect, beforeAll, beforeEach } from 'vitest'
 import db from './connection'
 import { getAllEvents, getUserById } from './db'
+import request from 'supertest'
+import server from '../server.ts'
+import { Events } from '../../models/flightplan.ts'
 
 beforeAll(async () => {
   await db.migrate.latest()
@@ -27,5 +30,63 @@ describe('get all events', () => {
     expect(events[0]).toHaveProperty('created_by')
     expect(events[1]).toHaveProperty('end_time')
     expect(events[1]).toHaveProperty('notes')
+  })
+})
+
+describe('Post/Patch/Delete, /api/v1/events', () => {
+  it('should return 201 when creating a new event', async () => {
+    const id = 2
+    const fakeEvent: Events = {
+      id: 2,
+      tripId: 1,
+      createdBy: '1',
+      date: '15/8/2024',
+      startTime: '1100',
+      endTime: '1300',
+      description: 'Test Test',
+      note: 'Testing Test',
+    }
+    const respone = await request(server)
+      .post(`/api/v1/events/${id}`)
+      .send(fakeEvent)
+    expect(respone.status).toBe(201)
+  })
+  it('Should return 200 when updating an event', async () => {
+    const id = 2
+    const existingEvent: Events = {
+      id: 2,
+      tripId: 1,
+      createdBy: '1',
+      date: '15/8/2024',
+      startTime: '1100',
+      endTime: '1300',
+      description: 'Test Test',
+      note: 'Testing Test',
+    }
+
+    const updatedEvent: Partial<Events> = {
+      date: '16/8/2024',
+      startTime: '1200',
+      endTime: '1400',
+      description: 'Updated Test',
+      note: 'Updated Testing Test',
+    }
+    await request(server)
+      .post(`/api/v1/events/${id}`)
+      .send(existingEvent)
+      .expect(201)
+
+    const respone = await request(server)
+      .patch(`/api/v1/events/${id}`)
+      .send(updatedEvent)
+
+    expect(respone.status).toBe(200)
+  })
+  it('Should delete the event by id', async () => {
+    const id = 2
+
+    const deleteEvent = await request(server).delete(`/api/v1/events/${id}`)
+
+    expect(deleteEvent.status).toBe(200)
   })
 })
