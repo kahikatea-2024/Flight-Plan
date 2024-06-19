ce53bf3a86faa38b8ca1aecdf8a13b452ac406b4
Aimee Kilmartin <aimeekilmartin@gmail.com>
2024-06-19 15:50:06 +1200

    styling and structure of log in form

--- client/pages/LogIn.tsx
+++ client/pages/LogIn.tsx
@@ -3,24 +3,30 @@ export function LogIn() {
   return (
     <section>
       <div className="container is-fluid">
-        <form className="field-is-horizontal is-centered">
-          <div className="field">
-            <label className="label is-medium">Email</label>
-            <div className="control">
-              <input type="text" className="input" placeholder="Email" />
-            </div>
+        <h1 className="title has-text-centered">Welcome to TripHive</h1>
+        <p className="has-text-centered mb-5">Sign Up or Log In</p>
+        <div className="columns is-fluid">
+          <div className="column is-half is-offset-one-quarter">
+            <form className="field-is-horizontal is-centered">
+              <div className="field">
+                <label className="label is-medium">Email</label>
+                <div className="control">
+                  <input type="text" className="input" placeholder="Email" />
+                </div>
+              </div>
+              <div className="field">
+                <label className="label is-medium">Password</label>
+                <div className="control">
+                  <input type="text" className="input" placeholder="Password" />
+                </div>
+              </div>
+              <div className="field is-grouped is-grouped-centered mt-6">
+                <button className="button is-primary">Log In</button>
+                <button className="button is-primary is-light">Sign Up</button>
+              </div>
+            </form>
           </div>
-          <div className="field">
-            <label className="label is-medium">Password</label>
-            <div className="control">
-              <input type="text" className="input" placeholder="Password" />
-            </div>
-          </div>
-          <div className="field is-grouped is-grouped-centered mt-6">
-            <button className="button is-primary">Log In</button>
-            <button className="button is-secondary">Sign Up</button>
-          </div>
-        </form>
+        </div>
       </div>
     </section>
   )
