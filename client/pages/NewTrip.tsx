fc18abbda55a53617de057903a66ce96c1e1e670
Brad Craig <brad.a.craig@gmail.com>
2024-06-20 11:48:29 +1200

    render trip name form

--- client/pages/NewTrip.tsx
+++ client/pages/NewTrip.tsx
@@ -1,3 +1,21 @@
 export function NewTrip() {
-  return <div>New Trip Page</div>
+  return (
+    <section className="section">
+      <div className="container is-fluid">
+        <h1 className="title has-text-centered">Start a new Trip</h1>
+        <div className="columns">
+          <div className="column is-half is-offset-one-quarter">
+            <form>
+              <div className="field">
+                <label className="label">Trip Name</label>
+                <div className="control">
+                  <input type="text" className="input" />
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
