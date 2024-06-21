026613e87d466b0d0a5eec79171ac8ddad59cf49
Brad Craig <brad.a.craig@gmail.com>
2024-06-21 18:04:51 +1200

    started writing MyFriends page

--- client/components/AddFriend.tsx
+++
@@ -1,8 +0,0 @@
-export function AddFriend() {
-  return (
-    <div>
-      <h2>Add Friend</h2>
-      <h3>Enter email to add to My Friends</h3>
-    </div>
-  )
-}
--- client/pages/MyFriends.tsx
+++ client/pages/MyFriends.tsx
@@ -1,3 +1,12 @@
 export function MyFriends() {
-  return <div>My Friends Page</div>
+  return (
+    <div className="container is-fluid">
+      <h2>Add Friend</h2>
+      <h3>Enter email to add to My Friends</h3>
+      <form>
+        <input type="text" />
+        <button className="button is-primary">Add Friend</button>
+      </form>
+    </div>
+  )
 }
