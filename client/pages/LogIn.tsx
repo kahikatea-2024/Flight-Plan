be845f7ba6f23fa1d3e223651ae6d51c9fcbe0a3
Regie Malonzo <regiemalonzo@gmail.com>
2024-06-22 22:52:17 +1200

    import Link

--- client/pages/LogIn.tsx
+++ client/pages/LogIn.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { AddNewUser } from '../components/AddNewUser'
 import { useAuth } from '../context/UserContext'
 import { useState } from 'react'
