053a0b11b43a370afb1051107d94086173b906b5
Aimee Kilmartin <aimeekilmartin@gmail.com>
2024-06-25 11:25:23 +1200

    two tests passing, need to add more conditions

--- client/tests/addEvent.test.tsx
+++ client/tests/addEvent.test.tsx
@@ -40,67 +40,37 @@ const fakeData = [
   },
 ]
 
-const fakeNewEvent = {
-  id: 4,
-  tripId: 123,
-  date: '04-08-2024',
-  description: 'bungy jump',
-  endTime: '10am',
-  notes: 'be brave',
-  startTime: '5am',
-}
+// const fakeNewEvent = {
+//   id: 4,
+//   tripId: 123,
+//   date: '04-08-2024',
+//   description: 'bungy jump',
+//   endTime: '10am',
+//   notes: 'be brave',
+//   startTime: '5am',
+// }
 
 describe('The days events list', () => {
   it('shows some events', async () => {
     const scope = nock('http://localhost')
       .get('/api/v1/events/date/123/04-08-2024')
       .reply(200, fakeData)
-    ;('/tripId/:id/date/:date')
+    // ;('/tripId/:id/date/:date')
     const screen = setupApp('/tripId/123/date/04-08-2024') //front end route
 
     const description = await screen.findByText('surf some waves')
     expect(description.textContent).toBe('surf some waves')
     expect(scope.isDone()).toBe(true)
   })
+
+  it('should render form', async () => {
+    const scope = nock('http://localhost')
+      .get('/api/v1/events/date/123/04-08-2024')
+      .reply(200, fakeData)
+
+    const screen = setupApp('/tripId/123/date/04-08-2024') //front end route
+
+    //change getByLabelText to findByLabelText when it's async
+    expect(await screen.findByLabelText('Event Title')).toBeInTheDocument()
+  })
 })
-
-// describe('Add Event to date', () => {
-//   it('should render form', () => {
-//     const handleSubmit = vi.fn()
-//     renderComponent(<AddEvent handleSubmit={handleSubmit} />)
-
-//     expect(screen.getByLabelText('title')).toBeInTheDocument()
-//     expect(screen.getByLabelText('startHour')).toBeInTheDocument()
-//     expect(screen.getByLabelText('Last Name')).toBeInTheDocument()
-//     expect(screen.getByLabelText('Visible to everyone')).toBeInTheDocument()
-//   })
-
-//   it('event handler should be called when form is submitted', async () => {
-//     const handleSubmit = vi.fn((form: Event) => {
-//       expect(form).toMatchObject({
-//         title: "testTitle",
-//         startHour: ,
-//         startMinutes: ' ',
-//         startAMPM: ' ',
-//         endHour: ' ',
-//         endMinutes: ' ',
-//         endAMPM: ' ',
-//         note: ' ',
-//       })
-//     })
-
-//     const { user } = renderComponent(
-//       <ProfileForm handleSubmit={handleSubmit} />,
-//     )
-
-//     await user.type(screen.getByLabelText('Nickname *'), 'dummy-nickname')
-//     await user.type(screen.getByLabelText('First Name *'), 'dummy-firstname')
-//     await user.type(screen.getByLabelText('Last Name'), 'dummy-lastname')
-//     await user.click(screen.getByLabelText('Visible to everyone'))
-
-//     const form = screen.getByRole('button', { name: 'Save' })
-//     await user.click(form)
-
-//     expect(handleSubmit).toHaveBeenCalled()
-//   })
-// })
