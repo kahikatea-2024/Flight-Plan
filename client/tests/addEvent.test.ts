48f3613c552a881fe7cf70e43a31eeedd7f3491b
Aimee Kilmartin <aimeekilmartin@gmail.com>
2024-06-24 16:21:55 +1200

    first test passing

--- client/tests/addEvent.test.ts
+++ client/tests/addEvent.test.ts
@@ -13,39 +13,37 @@ describe('The days events list', () => {
   it('shows some events', async () => {
     const scope = nock('http://localhost')
       .get('/api/v1/events/date/04-08-2024')
-      .reply(200, {
-        events: [
-          {
-            id: 1,
-            date: '04-08-2024',
-            description: 'surf some waves',
-            endTime: '10am',
-            notes: 'wetsuit',
-            startTime: '5am',
-          },
-          {
-            id: 2,
-            date: '04-08-2024',
-            description: 'eat',
-            endTime: '6pm',
-            notes: 'bami goreng',
-            startTime: '5pm',
-          },
-          {
-            id: 3,
-            date: '04-08-2024',
-            description: 'swim',
-            endTime: '12pm',
-            notes: 'sunblock',
-            startTime: '7am',
-          },
-        ],
-      })
+      .reply(200, [
+        {
+          id: 1,
+          date: '04-08-2024',
+          description: 'surf some waves',
+          endTime: '10am',
+          notes: 'wetsuit',
+          startTime: '5am',
+        },
+        {
+          id: 2,
+          date: '04-08-2024',
+          description: 'eat',
+          endTime: '6pm',
+          notes: 'bami goreng',
+          startTime: '5pm',
+        },
+        {
+          id: 3,
+          date: '04-08-2024',
+          description: 'swim',
+          endTime: '12pm',
+          notes: 'sunblock',
+          startTime: '7am',
+        },
+      ])
 
     const screen = setupApp('/date/04-08-2024') //front end route
 
     const description = await screen.findByText('surf some waves')
-    expect(description).toBe('surf some waves')
+    expect(description.textContent).toBe('surf some waves')
     expect(scope.isDone()).toBe(true)
   })
 })
