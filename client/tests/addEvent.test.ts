56bedf0bd2e8ba69f39ea802bed4d45074d92aa5
Aimee Kilmartin <aimeekilmartin@gmail.com>
2024-06-24 17:48:25 +1200

    test passing with updated routes

--- client/tests/addEvent.test.ts
+++ client/tests/addEvent.test.ts
@@ -9,38 +9,43 @@ beforeAll(() => {
 
 //nock other username calls first
 
+const fakeData = [
+  {
+    id: 1,
+    tripId: 123,
+    date: '04-08-2024',
+    description: 'surf some waves',
+    endTime: '10am',
+    notes: 'wetsuit',
+    startTime: '5am',
+  },
+  {
+    id: 2,
+    tripId: 123,
+    date: '04-08-2024',
+    description: 'eat',
+    endTime: '6pm',
+    notes: 'bami goreng',
+    startTime: '5pm',
+  },
+  {
+    id: 3,
+    tripId: 123,
+    date: '04-08-2024',
+    description: 'swim',
+    endTime: '12pm',
+    notes: 'sunblock',
+    startTime: '7am',
+  },
+]
+
 describe('The days events list', () => {
   it('shows some events', async () => {
     const scope = nock('http://localhost')
-      .get('/api/v1/events/date/04-08-2024')
-      .reply(200, [
-        {
-          id: 1,
-          date: '04-08-2024',
-          description: 'surf some waves',
-          endTime: '10am',
-          notes: 'wetsuit',
-          startTime: '5am',
-        },
-        {
-          id: 2,
-          date: '04-08-2024',
-          description: 'eat',
-          endTime: '6pm',
-          notes: 'bami goreng',
-          startTime: '5pm',
-        },
-        {
-          id: 3,
-          date: '04-08-2024',
-          description: 'swim',
-          endTime: '12pm',
-          notes: 'sunblock',
-          startTime: '7am',
-        },
-      ])
-
-    const screen = setupApp('/date/04-08-2024') //front end route
+      .get('/api/v1/events/date/123/04-08-2024')
+      .reply(200, fakeData)
+    ;('/tripId/:id/date/:date')
+    const screen = setupApp('/tripId/123/date/04-08-2024') //front end route
 
     const description = await screen.findByText('surf some waves')
     expect(description.textContent).toBe('surf some waves')
