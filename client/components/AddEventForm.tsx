b6151dba85b8cf72886552fd410b2db5eb8fb929
Aimee Kilmartin <aimeekilmartin@gmail.com>
2024-08-30 23:49:26 +1200

    update event form to use components

--- client/components/AddEventForm.tsx
+++ client/components/AddEventForm.tsx
@@ -2,6 +2,11 @@ import { useState } from 'react'
 import { EventData, Events } from '../../models/flightplan'
 import { addEvent, getEvents } from '../apis/events'
 import { combineTimeAndDay, eventFormValidation } from '../utilities/eventTimes'
+import { FormText } from './FormText'
+import { FormLabel } from './formComponents/FormLabel'
+import { FormRadio } from './formComponents/FormRadio'
+import { FormSelect } from './formComponents/FormSelect'
+import { FormTime } from './formComponents/FormTime'
 
 interface AddEventProps {
   date: string
@@ -109,238 +114,139 @@ export function AddEvent({ date, tripId, setEvents }: AddEventProps) {
     <section>
       <div className="container is-fluid is-centered">
         <div className="columns is-fluid">
-          <div className="column">
-            <h2 className="is-size-2 has-text-centered has-text-primary">
-              Add An Event
-            </h2>
+          {/* <div className="column"> */}
+          <h2 className="is-size-2 has-text-centered has-text-primary">
+            Add An Event
+          </h2>
+          <div>
             <form
               onSubmit={handleSubmit}
               className="field-is-horizontal is-centered"
             >
-              <div className="field level">
-                <label className="label level-left" htmlFor="description">
-                  Event Name
-                </label>
-                <div className="control level-item">
-                  <input
-                    type="text"
-                    className="input"
-                    placeholder="Event Name"
-                    name="description"
-                    id="description"
-                    value={formData.description}
-                    onChange={handleChange}
-                  />
-                  {formErrors.description && (
-                    <div className="error">{formErrors.description}</div>
-                  )}
-                </div>
+              <div className="field is-horizontal">
+                <FormLabel label="Title" id={'description'} />
+                <FormText
+                  placeholder="Event Title"
+                  name={'description'}
+                  id={'description'}
+                  value={formData.description}
+                  handleChange={handleChange}
+                  formErrors={formErrors.description}
+                />
               </div>
-              <div className="field level">
-                <label className="label level-left" htmlFor="location">
-                  Event Location
-                </label>
-                <div className="control level-item">
-                  <input
-                    type="text"
-                    className="input"
-                    placeholder="Event Location"
-                    name="location"
-                    id="location"
-                    value={formData.location}
-                    onChange={handleChange}
-                  />
-                  {formErrors.location && (
-                    <div className="error">{formErrors.location}</div>
-                  )}
+              <div className="field is-horizontal">
+                <FormLabel label="Location" id={'location'} />
+                <FormText
+                  placeholder="Event Location"
+                  name={'location'}
+                  id={'location'}
+                  value={formData.location}
+                  handleChange={handleChange}
+                  formErrors={formErrors.location}
+                />
+              </div>
+
+              <div className="field is-horizontal">
+                <div className="field-label">
+                  <p className="label">Event Type</p>
+                </div>
+                <div className="field-body">
+                  <div className="field is-narrow">
+                    <div className="control">
+                      <FormRadio
+                        name={'type'}
+                        id={'Event'}
+                        value={'Event'}
+                        check={formData.type}
+                        handleChange={handleChange}
+                      />
+                      <FormRadio
+                        name={'type'}
+                        id={'Flight'}
+                        value={'Flight'}
+                        check={formData.type}
+                        handleChange={handleChange}
+                      />
+                      <FormRadio
+                        name={'type'}
+                        id={'Accommodation'}
+                        value={'Accommodation'}
+                        check={formData.type}
+                        handleChange={handleChange}
+                      />
+                    </div>
+                  </div>
                 </div>
               </div>
 
-              <div className="control level field is-horizontal">
-                <div className="level-left">
-                  <div className="field-label level field is-horizontal">
-                    Event Type
-                  </div>
-                  <span className="radio level-item pr-4 pl-4">
-                    <label
-                      className="radio field is-horizontal"
-                      htmlFor="Event"
-                    >
-                      <input
-                        type="radio"
-                        name="type"
-                        id="Event"
-                        value="Event"
-                        checked={formData.type === 'Event'}
-                        onChange={handleChange}
-                      />
-                      Event
-                    </label>
-                    <label
-                      className="radio field is-horizontal"
-                      htmlFor="Flight"
-                    >
-                      <input
-                        type="radio"
-                        name="type"
-                        id="Flight"
-                        value="Flight"
-                        checked={formData.type === 'Flight'}
-                        onChange={handleChange}
-                      />
-                      Flight
-                    </label>
-                    <label
-                      className="radio field is-horizontal"
-                      htmlFor="Accommodation"
-                    >
-                      <input
-                        type="radio"
-                        name="type"
-                        id="Accommodation"
-                        value="Accommodation"
-                        checked={formData.type === 'Accommodation'}
-                        onChange={handleChange}
-                      />
-                      Accommodation
-                    </label>
-
-                    {formErrors.type && (
-                      <span className="error">{formErrors.type}</span>
-                    )}
-                  </span>
-                </div>
-              </div>
-              <div className="field is-horizontal ">
-                <label className="field-label level-left " htmlFor="startHour">
-                  Event Start
-                </label>
-                <div className="field-body is-flex">
-                  <div className="grouped-wrapper">
-                    <input
-                      className="input"
-                      type="number"
-                      placeholder="00"
-                      name="startHour"
-                      id="startHour"
-                      value={formData.startHour}
-                      onChange={handleChange}
-                    />
-                    {formErrors.startHour && (
-                      <div className="error">{formErrors.startHour}</div>
-                    )}
-                  </div>
-                  <div className="grouped-wrapper">
-                    <label className="label " htmlFor="startMinutes">
-                      <input
-                        className="input"
-                        type="number"
-                        placeholder="00"
-                        name="startMinutes"
-                        id="startMinutes"
-                        value={formData.startMinutes}
-                        onChange={handleChange}
-                      />
-                      {formErrors.startMinutes && (
-                        <div className="error">{formErrors.startMinutes}</div>
-                      )}
-                    </label>
-                  </div>
-                  <div className=" grouped-wrapper">
-                    <span className="select">
-                      <label className="label " htmlFor="startAMPM">
-                        <select
-                          name="startAMPM"
-                          id="startAMPM"
-                          value={formData.startAMPM}
-                          onChange={handleChange}
-                        >
-                          <option>Select</option>
-                          <option>AM</option>
-                          <option>PM</option>
-                        </select>
-                        {formErrors.startAMPM && (
-                          <span className="error">{formErrors.startAMPM}</span>
-                        )}
-                      </label>
-                    </span>
-                  </div>
-                </div>
-              </div>
-              <div className="field is-horizontal ">
-                <label className="field-label level-left" htmlFor="endHour">
-                  Event End
-                </label>
-                <div className="field-body is-flex">
-                  <div className="grouped-wrapper">
-                    <input
-                      className="input"
-                      type="number"
-                      placeholder="00"
-                      name="endHour"
-                      id="endHour"
-                      value={formData.endHour}
-                      onChange={handleChange}
-                    />
-                    {formErrors.endHour && (
-                      <div className="error">{formErrors.endHour}</div>
-                    )}
-                  </div>
-                  <div className="grouped-wrapper">
-                    <label className="label " htmlFor="endMinutes">
-                      <input
-                        className="input"
-                        type="number"
-                        placeholder="00"
-                        name="endMinutes"
-                        id="endMinutes"
-                        value={formData.endMinutes}
-                        onChange={handleChange}
-                      />
-                      {formErrors.endMinutes && (
-                        <div className="error">{formErrors.endMinutes}</div>
-                      )}
-                    </label>
-                  </div>
-                  <div className=" grouped-wrapper">
-                    <span className="select">
-                      <label className="label " htmlFor="endAMPM">
-                        <select
-                          name="endAMPM"
-                          id="endAMPM"
-                          value={formData.endAMPM}
-                          onChange={handleChange}
-                        >
-                          <option>Select</option>
-                          <option>AM</option>
-                          <option>PM</option>
-                        </select>
-                        {formErrors.endAMPM && (
-                          <span className="error">{formErrors.endAMPM}</span>
-                        )}
-                      </label>
-                    </span>
-                  </div>
-                </div>
-              </div>
-              <div className="field level">
-                <label className="label level-left" htmlFor="note">
-                  Event Note
-                </label>
-                <div className="control level-item">
-                  <input
-                    type="text"
-                    className="input"
-                    placeholder="Event Note"
-                    name="note"
-                    id="note"
-                    value={formData.note}
-                    onChange={handleChange}
+              <div className="field is-horizontal">
+                <FormLabel label="Start Time" id={'startTime'} />
+                <div className="field-body is-horizontal">
+                  <FormTime
+                    placeholder="00"
+                    name={'startHour'}
+                    id={'startHour'}
+                    value={formData.startHour}
+                    handleChange={handleChange}
+                    formErrors={formErrors.startHour}
                   />
-                  {formErrors.note && (
-                    <div className="error">{formErrors.note}</div>
-                  )}
+                  <FormTime
+                    placeholder="00"
+                    name={'startMinutes'}
+                    id={'startMinutes'}
+                    value={formData.startMinutes}
+                    handleChange={handleChange}
+                    formErrors={formErrors.startMinutes}
+                  />
+                  <FormSelect
+                    name={'startAMPM'}
+                    id={'startAMPM'}
+                    value={formData.startAMPM}
+                    handleChange={handleChange}
+                    formErrors={formErrors.startAMPM}
+                  />
                 </div>
+              </div>
+
+              <div className="field is-horizontal">
+                <FormLabel label="End Time" id={'endTime'} />
+                <div className="field-body is-horizontal">
+                  <FormTime
+                    placeholder="00"
+                    name={'endHour'}
+                    id={'endHour'}
+                    value={formData.endHour}
+                    handleChange={handleChange}
+                    formErrors={formErrors.endHour}
+                  />
+                  <FormTime
+                    placeholder="00"
+                    name={'endMinutes'}
+                    id={'endMinutes'}
+                    value={formData.endMinutes}
+                    handleChange={handleChange}
+                    formErrors={formErrors.endMinutes}
+                  />
+                  <FormSelect
+                    name={'endAMPM'}
+                    id={'endAMPM'}
+                    value={formData.endAMPM}
+                    handleChange={handleChange}
+                    formErrors={formErrors.endAMPM}
+                  />
+                </div>
+              </div>
+
+              <div className="field is-horizontal">
+                <FormLabel label="Note" id={'note'} />
+                <FormText
+                  placeholder="Event note"
+                  name={'note'}
+                  id={'note'}
+                  value={formData.note}
+                  handleChange={handleChange}
+                  formErrors={formErrors.note}
+                />
               </div>
 
               <div className="field is-grouped is-grouped-centered mt-4">
