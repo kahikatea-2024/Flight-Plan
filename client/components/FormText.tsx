18a0d7b0b634a7b718672655f2265a23210a6e04
Aimee Kilmartin <aimeekilmartin@gmail.com>
2024-08-30 13:47:59 +1200

    add form text component

---
+++ client/components/FormText.tsx
@@ -0,0 +1,31 @@
+interface Props {
+  placeholder: string
+  name: string
+  id: string
+  value: string
+  handleChange: React.ChangeEventHandler<HTMLInputElement>
+  formErrors: string
+}
+
+export function FormText(props: Props) {
+  const { placeholder, name, id, value, handleChange, formErrors } = props
+
+  return (
+    <div className="field-body">
+      <div className="field is-expanded">
+        <div className="control">
+          <input
+            type="text"
+            className="input"
+            placeholder={placeholder}
+            name={name}
+            id={id}
+            value={value}
+            onChange={handleChange}
+          />
+        </div>
+        <p>{formErrors && <div className="error">{formErrors}</div>}</p>
+      </div>
+    </div>
+  )
+}
